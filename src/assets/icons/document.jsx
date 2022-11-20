567ed19e1b829955478c6fbe966d03f1cd04eb10
Dinesh Somaraju <dinesh99639@gmail.com>
2022-11-20 11:05:13 +0530

    Update icons size

--- src/assets/icons/document.jsx
+++ src/assets/icons/document.jsx
@@ -2,8 +2,8 @@ const DocumentIcon = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="23"
-      height="23"
+      width="21"
+      height="21"
       fill={props.color}
       viewBox="0 0 16 16"
     >
