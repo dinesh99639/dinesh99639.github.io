2fe309eb14cbcd0fee92faecb12b18b5d336c610
Dinesh Somaraju <dinesh@upsure.io>
2026-01-03 22:19:01 +0530

    refactor: center align navbar desktop links and standardize project entry file extensions

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -43,7 +43,7 @@ export function Navbar() {
 
                     {/* Desktop Menu */}
                     <div className="hidden md:block">
-                        <div className="ml-10 flex items-baseline space-x-4">
+                        <div className="ml-10 flex items-center space-x-4">
                             {navLinks.map((link) => (
                                 <a
                                     key={link.name}
